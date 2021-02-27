c1b14c9bcd1b1336f38984bd5bdd13f7f67ce10c
Zhaisan <zhaisan.moon@gmail.com>
2021-02-27 18:06:06 +0600

    hello

---
+++ js/scrip.js
@@ -0,0 +1,13 @@
+userName = 'Zhasan';
+userAge = 20;
+console.log( `hello, ${userName } ${userAge}`);
+var student = {
+    name: "all"
+};
+var teacher = {
+    name: "kun"
+};
+var time = {
+    life: "hello"
+};
+console.log('time.hello', time.life);
